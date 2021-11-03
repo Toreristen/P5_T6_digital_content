423e40636dd47d549469ffaab210ceacee51582d
LauraLorentzen <lauramlorentzen@gmail.com>
2021-11-03 10:28:27 +0100

    Tilføjet funktionallitet til se mere knap

---
+++ javascript/laura.js
@@ -0,0 +1,10 @@
+
+
+function dropDrown(){
+  let seMere = document.getElementById("seMere");
+  if (seMere.style.display === "none"){
+    seMere.style.display = "block";
+  } else {
+    seMere.style.display = "none";
+  }
+}
