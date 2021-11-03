ecb56d45903ed7f67683da94b6357ed279c6876b
Toreristen <82028481+Toreristen@users.noreply.github.com>
2021-11-03 14:22:39 +0100

    animation added to menulist

--- javascript/tore.js
+++ javascript/tore.js
@@ -1,3 +1,42 @@
 console.log("works")
 
+const hamburger = document.querySelector("#hamburger");
+const menu = document.querySelector(".nav__list-container");
+const menuItems = document.querySelectorAll(".nav__list-item");
 
+window.addEventListener('DOMContentLoaded', (event) => {
+    menu.style.visibility = "hidden";
+})
+
+
+function toggleMenu(hamburger) {
+    console.log("function works")
+    if (menu.style.visibility === "hidden") {
+        menu.classList.add("showMenu");
+        menu.classList.remove("closeMenu");
+        menu.style.visibility = "visible";
+        menuItems.style.visibility = "visible";
+        //insert animation
+    }else {
+        menu.style.visibility = "hidden";
+        menu.classList.remove("showMenu");
+        menu.classList.add("closeMenu");
+        //menuItems.style.visibility = "hidden";
+    }
+}
+
+hamburger.addEventListener("click", toggleMenu)
+
+
+
+
+
+
+
+
+
+
+let array = ["hej", "med", "dig"];
+for (let i = 0; i < array.length; i++) {
+
+}
