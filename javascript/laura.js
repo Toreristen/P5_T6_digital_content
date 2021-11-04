3a0f0af72822e36ef48c76e2f2e5d5f29c2a0e2e
LauraLorentzen <lauramlorentzen@gmail.com>
2021-11-04 11:06:26 +0100

    JS
    Ændret text,  drop down menu

--- javascript/laura.js
+++ javascript/laura.js
@@ -1,10 +1,21 @@
-
+let seMere;
+let points;
+let buttonText;
 
 function dropDrown(){
-  let seMere = document.getElementById("mere");
-  if (seMere.style.display === "none"){
+  if (seMere.style.display == "none"){
     seMere.style.display = "block";
+    points.style.display = "none";
   } else {
     seMere.style.display = "none";
+    points.style.display = "block";
   }
 }
+
+window.addEventListener("DOMContentLoaded", (event)=> {
+  seMere = document.getElementById("mere");
+  points = document.getElementById("points");
+  buttonText = document.getElementById("buttonText");
+  seMere.style.display = "none";
+  point.style.display = "none";
+})
