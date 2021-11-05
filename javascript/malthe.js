40e3bd9fab5341e3223bd3ed94627a1016bf6a58
Donspaghett <83805630+Donspaghett@users.noreply.github.com>
2021-11-05 09:02:26 +0100

    Begyndt på JS

---
+++ javascript/malthe.js
@@ -0,0 +1,13 @@
+console.log("virker");
+let hiddenTxt = document.getElementById('hidden-txt');
+let showParagraph = document.queryselector(".project__button-arrow");
+
+window.addEventListener('DOMContentLoaded', (event) =>{
+  hiddenTxt.style.display = "none";
+})
+
+function showTxt() {
+  if (hiddenTxt.style.display === "none") {
+    
+  }
+}
