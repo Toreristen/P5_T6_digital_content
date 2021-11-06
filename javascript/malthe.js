9b722dd154d4b0fde157d8b0f11b6a5d65d7ae0a
Donspaghett <83805630+Donspaghett@users.noreply.github.com>
2021-11-06 20:40:11 +0100

    java script færdig

--- javascript/malthe.js
+++ javascript/malthe.js
@@ -1,13 +1,28 @@
-console.log("virker");
-let hiddenTxt = document.getElementById('hidden-txt');
-let showParagraph = document.queryselector(".project__button-arrow");
+let hiddenTxt = document.getElementById("hidden-txt");
+let showParagraph = document.getElementById("se-mere-arrow");
 
 window.addEventListener('DOMContentLoaded', (event) =>{
   hiddenTxt.style.display = "none";
 })
 
 function showTxt() {
+  //"if" viser teksten. "else" skjuler den.
+
   if (hiddenTxt.style.display === "none") {
-    
+    console.log("funktion virker");
+    hiddenTxt.style.display = "initial";
   }
+  else {
+    hiddenTxt.style.display = "none";
+  }
+}
+
+
+//pensum ting
+
+let tropiskfrugt = ["mango", "passionsfrugt", "papaya"];
+
+//loop
+for (let i = 0; i < tropiskfrugt.length; i++) {
+  console.log(tropiskfrugt[i]);
 }
