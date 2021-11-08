f2c5bbac2752b2656dbfe43f3d1521b4af49ec99
FarissCreative <83574304+FarissCreative@users.noreply.github.com>
2021-11-08 11:13:25 +0100

    corrected error in js file

--- javascript/laura.js
+++ javascript/laura.js
@@ -1,13 +1,14 @@
-window.addEventListener("DOMContentLoaded", (event)=> {
-  let seMere = document.getElementById("mere");
-  let points = document.getElementById("points");
-  let buttonText = document.getElementById("buttonText");
+let seMere, points, buttonText;
+window.addEventListener("DOMContentLoaded", (event) => {
+  seMere = document.getElementById("mere");
+  points = document.getElementById("points");
+  buttonText = document.getElementById("buttonText");
   seMere.style.display = "none";
   points.style.display = "none";
 })
 
-function dropDrown(){
-  if (seMere.style.display == "none"){
+function dropDrown() {
+  if (seMere.style.display == "none") {
     seMere.style.display = "block";
     points.style.display = "none";
   }
@@ -18,8 +19,8 @@ function dropDrown(){
 }
 
 //pensum
-let test = ["et","to","tre"];
+let test = ["et", "to", "tre"];
 //loop
-for ( let i = 0; i < test.length; i++) {
+for (let i = 0; i < test.length; i++) {
   console.log(test[i]);
 }
