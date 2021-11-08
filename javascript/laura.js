4a1ca8fb3424547dbbf710a7edb31a622b11a7c7
LauraLorentzen <lauramlorentzen@gmail.com>
2021-11-08 10:47:53 +0100

    js

--- javascript/laura.js
+++ javascript/laura.js
@@ -1,29 +1,25 @@
-let seMere;
-let points;
-let buttonText;
+window.addEventListener("DOMContentLoaded", (event)=> {
+  let seMere = document.getElementById("mere");
+  let points = document.getElementById("points");
+  let buttonText = document.getElementById("buttonText");
+  seMere.style.display = "none";
+  points.style.display = "none";
+})
 
 function dropDrown(){
   if (seMere.style.display == "none"){
     seMere.style.display = "block";
     points.style.display = "none";
-  } else {
+  }
+  else {
     seMere.style.display = "none";
     points.style.display = "block";
   }
 }
 
-window.addEventListener("DOMContentLoaded", (event)=> {
-  seMere = document.getElementById("mere");
-  points = document.getElementById("points");
-  buttonText = document.getElementById("buttonText");
-  seMere.style.display = "none";
-  points.style.display = "none";
-})
-
 //pensum
 let test = ["et","to","tre"];
 //loop
-for (var i = 0;
-  i < test.length; i++) {
+for ( let i = 0; i < test.length; i++) {
   console.log(test[i]);
 }
