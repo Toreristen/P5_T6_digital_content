424956a2b853ad86471843743d2d102911e88219
Toreristen <82028481+Toreristen@users.noreply.github.com>
2021-11-11 16:39:41 +0100

    js

--- javascript/tore.js
+++ javascript/tore.js
@@ -5,7 +5,7 @@
 const hamburger = document.querySelector("#hamburger");
 const menu = document.querySelector(".nav__list-container");
 const menuItems = document.querySelectorAll(".nav__list-item");
-const closeMenu = document.querySelector(".nav__list-item--active")
+const closeMenu = document.querySelector(".nav__list-item--active");
 const closeIcon = document.querySelector(".nav__icon2");
 
 
@@ -20,7 +20,7 @@ function toggleMenu() {
     
     if (menu.style.visibility === "hidden") {
         menu.classList.add("showMenu");
-        menu.classList.remove("closeMenu");
+        //menu.classList.remove("closeMenu");
         menu.style.visibility = "visible";
         //menuItems.style.visibility = "visible";
         hamburger.style.display = "none";
@@ -30,7 +30,7 @@ function toggleMenu() {
     else {
         menu.style.visibility = "hidden";
         menu.classList.remove("showMenu");
-        menu.classList.add("closeMenu");
+        //menu.classList.add("closeMenu");
         closeIcon.style.display = "none";
         hamburger.style.display = "initial";
         //menuItems.style.visibility = "hidden";
@@ -45,7 +45,8 @@ closeIcon.addEventListener("click", toggleMenu);
 
 //Pensum kode der mangler:
 
-let array = ["hej", "med", "dig"];
-for (let i = 0; i < array.length; i++) {
-    console.log(array[i]);
+let hilsen = ["hej", "med", "dig"];
+
+for (let i = 0; i < hilsen.length; i++) {
+    console.log(hilsen[i]);
 }
